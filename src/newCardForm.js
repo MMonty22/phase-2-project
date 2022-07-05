0a7483a9510bd0ac858cbd08b27d49dbb3bccdb2
Michael <michael.c.montgomery@gmail.com>
2022-07-05 15:55:29 -0500

    adding states on NewCardForm

--- src/newCardForm.js
+++ src/newCardForm.js
@@ -1,6 +1,11 @@
-import React from "react";
+import React, {useState} from "react";
 
 function NewCardForm() {
+    const [playerName, setPlayerName] = useState("")
+    const [image, setImage] = useState("")
+    const [team, setTeam] = useState("")
+    const [playerPos, setPlayerPos] = useState("")
+
   return (
     <div></div>
   )
