074d6810cd1bdb4fae35cc14377bc4c0fc699a2d
Michael <michael.c.montgomery@gmail.com>
2022-07-05 15:37:22 -0500

    setting up remove favorite buttonon favorites component

--- src/Favorites.js
+++ src/Favorites.js
@@ -1,15 +1,19 @@
 import React from "react";
 
-function FavoriteList({card}) {
+function FavoriteList({card, removeFavorite}) {
     const {player, team, image, position} = card
 
+    function handleClick() {
+        removeFavorite(card)
+    }
+    
     return (
         <div className="card">
             <h3>{player}</h3>
             <img src={image} alt={player}/>
             <h4>{team}</h4>
             <p>{position}</p>
-            <button></button>
+            <button onClick={handleClick}>Remove as Favorite</button>
         </div>
     )
 }
