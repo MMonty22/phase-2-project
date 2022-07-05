de992803b7e14a02f371d01b1274072b688ec3ff
Michael <michael.c.montgomery@gmail.com>
2022-07-05 15:58:47 -0500

    adding form submit/POST

--- src/newCardForm.js
+++ src/newCardForm.js
@@ -1,15 +1,34 @@
 import React, {useState} from "react";
 
-function NewCardForm() {
+function NewCardForm({addCard}) {
     const [playerName, setPlayerName] = useState("")
     const [image, setImage] = useState("")
     const [team, setTeam] = useState("")
     const [playerPos, setPlayerPos] = useState("")
 
+    function handleSubmit(event) {
+        event.preventDefault()
+        const newCardObj = {
+            player: playerName,
+            image: image,
+            team: team,
+            position: playerPos
+        }
+        fetch('http://localhost:3000/baseballCards', {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(newCardObj)
+        })
+        .then(res => res.json())
+        .then((data) => addCard(data))
+    }
+
   return (
     <div>
         <h2 className="new-card">Add A Card</h2>
-        <form>
+        <form onSubmit={handleSubmit}>
             <label>Player Name</label>
             <input type="text" name="name" placeholder="Players full name" value={playerName} />
             <label>Image URL</label>
