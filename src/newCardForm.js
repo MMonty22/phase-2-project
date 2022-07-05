c254d8173ffa5d5dbcc9badeaa22fa7bfedd213b
Michael <michael.c.montgomery@gmail.com>
2022-07-05 15:56:53 -0500

    adding form DOM elements

--- src/newCardForm.js
+++ src/newCardForm.js
@@ -7,7 +7,20 @@ function NewCardForm() {
     const [playerPos, setPlayerPos] = useState("")
 
   return (
-    <div></div>
+    <div>
+        <h2 className="new-card">Add A Card</h2>
+        <form>
+            <label>Player Name</label>
+            <input type="text" name="name" placeholder="Players full name" value={playerName} />
+            <label>Image URL</label>
+            <input type="text" name="image" placeholder="Link to image" value={image} />
+            <label>Team Name</label>
+            <input type="text" name="team" placeholder="Ex: Chicago Cubs" value={team} />
+            <label>Player Position</label>
+            <input type="text" name="position" placeholder="Ex: OF" value={playerPos} />
+            <button type="submit">Add Card</button>
+            </form>
+    </div>
   )
 }
 
