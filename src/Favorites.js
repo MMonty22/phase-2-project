5b5968ea7e072cffba5e71e76f14e90e1a3aea9e
Michael <michael.c.montgomery@gmail.com>
2022-07-05 15:36:00 -0500

    adding DOM elements to FavoriteList

--- src/Favorites.js
+++ src/Favorites.js
@@ -1,9 +1,17 @@
 import React from "react";
 
-function Favorites() {
-  return (
-    <div></div>
-  )
+function FavoriteList({card}) {
+    const {player, team, image, position} = card
+
+    return (
+        <div className="card">
+            <h3>{player}</h3>
+            <img src={image} alt={player}/>
+            <h4>{team}</h4>
+            <p>{position}</p>
+            <button></button>
+        </div>
+    )
 }
 
-export default Favorites;
+export default FavoriteList
